094ed922d0329d36564a98421009c8413d5fd225
Sergi <sergijuliafarre@gmail.com>
2021-10-20 14:14:00 +0200

    Clean metadata

--- topological_mott_insulator/_nbdev.py
+++ topological_mott_insulator/_nbdev.py
@@ -5,7 +5,9 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 index = {"checkerboard_lattice_un": "00_core.ipynb",
          "checkerboard_lattice_4unitcell": "00_core.ipynb",
          "numbern": "00_core.ipynb",
-         "chern": "00_core.ipynb"}
+         "chern": "00_core.ipynb",
+         "plot_bonds": "00_core.ipynb",
+         "plot_lattice": "00_core.ipynb"}
 
 modules = ["core.py"]
 
