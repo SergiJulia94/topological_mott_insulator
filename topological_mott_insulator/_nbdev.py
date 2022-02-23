df5c795fa024639799e55638e4e463bdc8f0c074
lorenzocardarelli <lorenzocarda@gmail.com>
2022-02-23 16:18:40 +0100

    update

--- topological_mott_insulator/_nbdev.py
+++ topological_mott_insulator/_nbdev.py
@@ -3,12 +3,15 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"checkerboard_lattice_un": "00_Unrestricted.ipynb",
+         "checkerboardeightsites": "01_Restricted.ipynb",
+         "mfcheckb": "01_Restricted.ipynb",
          "numbern": "02_Utils.ipynb",
          "plot_bonds": "02_Utils.ipynb",
          "plot_lattice": "02_Utils.ipynb",
          "Rydberg_v3v4": "02_Utils.ipynb"}
 
 modules = ["unrestricted.py",
+           "restricted.py",
            "utils.py"]
 
 doc_url = "https://SergiJulia94.github.io/topological_mott_insulator/"
